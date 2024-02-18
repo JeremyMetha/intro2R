a291494bc53c3cf138e960efb0857bb137aa40ff
JeremyMetha <36722881+JeremyMetha@users.noreply.github.com>
2024-02-19 09:33:39 +1100

    Add files via upload

---
+++ intro part 1 framework.R
@@ -0,0 +1,96 @@
+# setting up
+
+setwd() # first, let's decide where our r stuf should end up.
+
+
+# using r as a calculator
+
+# comparisons
+
+#####
+# Challenge 1: Basic Driving
+
+#   a) Basic calculator time! Run the following in the CONSOLE:
+#   32*19 + 123,
+#   what do you get?
+
+#   b) Run the following in a SCRIPT: 11*34 + log(10)
+#   What happens? Do you get a value? Error? Warning?
+#   Can you fix it?
+#   c) Which one is larger, cos(10) or sin(10)? Can you write an
+#   expression to solve this?
+#####
+
+
+# functions
+
+
+#####
+# Challenge 2: Functions and Help
+
+#   a) What does the abs() function do? Access help.
+#      What is abs(-10)
+
+#   b) What is the square root of 11? Is there a function for this in R?
+
+#   c) How can we round numbers in R? Round 3.5 to the nearest integer.
+#      Is it 3 or 4?
+#####
+
+# variables and storing data
+
+# names can contain
+# letters
+# numbers
+# underscores
+# periods
+
+#names can't
+# contain spaces
+# start with a number or underscore
+
+# names are case sensitive
+
+#####
+# Challenge 3: Variables and Storing Data
+
+#   a) Try assigning some variables! Which of the following work as names?
+# min_height
+# _age
+# .mass
+# MaxLength
+# maxLength
+# widths2
+
+#   b) What will be the value of each variable below after each step?
+# height = 181.2
+# weight = 85
+# height = height ^2
+# weight = sqrt(weight) + height/2
+#####
+
+# managing your environment
+
+# vectors
+
+#####
+## Challenge 4: Vectors
+
+#   a) Create a vector with these numbers:
+#      1, 11, 111, 1111
+
+#   b) What is the square root of each of the numbers in A?
+#      Store these as another vector.
+
+#   c) Create a vector with the name of each person on your table.
+
+#####
+
+# expanding r with packages
+
+#####
+## Challenge 5: Packages
+
+# Install the tidyverse package onto your machines, then check if it's there and behaving using the library() function.
+
+#####
